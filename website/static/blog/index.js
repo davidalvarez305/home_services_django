6157a1f4bd85f2d32b2fc55c9a07a7c640332f7c
unknown <david@southfloridaathleticclub.com>
2022-11-21 03:27:56 -0500

    more js fun

--- website/static/blog/index.js
+++ website/static/blog/index.js
@@ -3,8 +3,22 @@ const user = document.getElementById("user");
 
 welcome.innerHTML = "Hey!";
 
-const data = JSON.parse(document.getElementById('current').textContent);
+function fetchData() {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((res) =>
+      res.json().then((data) => {
+        user.innerHTML = data.title;
+      })
+    )
+    .catch((err) => console.log(err));
+}
+
+const data = JSON.parse(document.getElementById("current").textContent);
 
 user.addEventListener("click", function (e) {
   user.innerHTML = data.name;
+});
+
+welcome.addEventListener("click", function (e) {
+  fetchData();
 });
