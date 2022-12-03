58501c1113e020e73b21994321f36bae81004661
unknown <david@southfloridaathleticclub.com>
2022-12-03 16:07:28 -0500

    getting marketing data

--- website/static/blog/index.js
+++ website/static/blog/index.js
@@ -3,6 +3,18 @@ var zipCodeInput = document.getElementById("widget-subscribe-form-email");
 
 var qs = new URLSearchParams(window.location.search);
 
+function getLeadChannel() {
+
+  // No referrer means the user accessed the website directly
+  if (!document.referrer) return "direct";
+
+  // If we get to this point, it means that document.referrer is not null
+  // If there's no paid traffic because 'medium' is empty -> it's organic
+  if (!qs.get('medium')) return "organic";
+
+  return "paid";
+}
+
 quoteButton.addEventListener("click", function (e) {
 
   qs.set('zip_code', zipCodeInput.value);
