5ee0e8235428970bdc48a3e66e3146c46a7cca55
unknown <david@southfloridaathleticclub.com>
2022-12-03 14:06:34 -0500

    rework for more consistency

--- website/static/blog/index.js
+++ website/static/blog/index.js
@@ -1,31 +1,13 @@
-function parseQueryStrings() {
-  var queryParams = {};
-
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
-
-    queryParams[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
-  }
-
-  return queryParams;
-}
-
 var quoteButton = document.getElementById("quote-button");
 var zipCodeInput = document.getElementById("widget-subscribe-form-email");
 
-var parsedQueryStrings = parseQueryStrings();
+var qs = new URLSearchParams(window.location.search);
 
 quoteButton.addEventListener("click", function (e) {
 
-  var zipCodeString = "?zip_code=" + zipCodeInput.value;
-
-  if (window.location.search.length !== 0) {
-    zipCodeString = window.location.search += "&zip_code=" + zipCodeInput.value;
-  }
+  qs.set('zip_code', zipCodeInput.value)
 
   window.location.replace(
-    "http://127.0.0.1:8000/get-a-quote/" + zipCodeString
+    "http://127.0.0.1:8000/get-a-quote/?" + qs.toString()
   );
 });
